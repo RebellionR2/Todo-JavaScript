40c4ab6713c8ab75595d17cc2073bf9b2b39f681
RebellionR2 <shubhamdubey240620@gmail.com>
2025-02-18 22:45:45 +0530

    Styling added

--- scripts.js
+++ scripts.js
@@ -3,6 +3,19 @@ let add=document.getElementById("Add");
 let list=document.getElementById("list");
 add.addEventListener("click", function (){
     let task=document.createElement("li");
-    task.innerHTML=input.value;
+    let contenttext=document.createElement('span');
+    contenttext.innerHTML=input.value;
+    task.appendChild(contenttext);
+    
+    let delbtn=document.createElement("button");
+    delbtn.innerHTML="❌";
+    delbtn.classList="delete";
+    delbtn.addEventListener("click", function(){
+        list.removeChild(task);
+    })
+
+    task.appendChild(delbtn);
     list.appendChild(task);
+
+
 })
